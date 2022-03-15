9fe79923bbb3583722a086aab433906e675d387b
NG <mashimaro514@gmail.com>
2022-03-15 21:16:58 +0800

    feat: project and team page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,80 @@
 <template lang="">
-  <div>
-    <h1>Projects</h1>
+  <div class="mt-4 ml-4">
+    <h1 class="subheader grey--text">
+      Projects
+    </h1>
+
+    <v-expansion-panels class="mt-5">
+      <v-expansion-panel
+          v-for="project in myProjects"
+          :key="project.title"
+      >
+        <v-expansion-panel-header>
+          {{ project.title }}
+        </v-expansion-panel-header>
+        
+        <v-expansion-panel-content>
+          <v-card flat>
+            <v-card-text class="px-4 py=- grey-text">
+              <div class="font-weight-bold">
+                Due by {{ project.due }}
+              </div>
+              <div>{{ project.content }}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 <script>
 export default {
-  name: "ProjectsView",
+	name: "ProjectsView",
+	data() {
+		return {
+			projects: [
+				{
+					title: "Design a new website",
+					person: "John",
+					due: "March 2022",
+					status: "ongoing",
+					content:
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi assumenda cupiditate, reiciendis suscipit quod laudantium, architecto itaque, odio ab beatae sapiente fugit. Sed id deleniti autem? Facere, doloribus eaque dolore, quam fuga iusto voluptatem odio tenetur dignissimos reiciendis molestias maiores? Esse nesciunt cum sint ipsam autem molestiae porro quod tenetur veritatis ratione quasi libero vitae, voluptatibus, ad quibusdam. Quos dolorum labore fugiat deserunt totam dolor voluptatibus quidem veritatis distinctio quia accusamus maxime nesciunt unde officiis similique iusto earum ipsa, eveniet eligendi dolores. Sunt quibusdam adipisci, suscipit nisi repellendus consequatur odio totam ullam explicabo voluptates voluptatem doloremque, aperiam modi saepe nam!",
+				},
+				{
+					title: "Code up the home page",
+					person: "Doe",
+					due: "September 2022",
+					status: "complete",
+					content:
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi assumenda cupiditate, reiciendis suscipit quod laudantium, architecto itaque, odio ab beatae sapiente fugit. Sed id deleniti autem? Facere, doloribus eaque dolore, quam fuga iusto voluptatem odio tenetur dignissimos reiciendis molestias maiores? Esse nesciunt cum sint ipsam autem molestiae porro quod tenetur veritatis ratione quasi libero vitae, voluptatibus, ad quibusdam. Quos dolorum labore fugiat deserunt totam dolor voluptatibus quidem veritatis distinctio quia accusamus maxime nesciunt unde officiis similique iusto earum ipsa, eveniet eligendi dolores. Sunt quibusdam adipisci, suscipit nisi repellendus consequatur odio totam ullam explicabo voluptates voluptatem doloremque, aperiam modi saepe nam!",
+				},
+				{
+					title: "Design video thumbnails",
+					person: "Chun Li",
+					due: "October 2022",
+					status: "overdue",
+					content:
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi assumenda cupiditate, reiciendis suscipit quod laudantium, architecto itaque, odio ab beatae sapiente fugit. Sed id deleniti autem? Facere, doloribus eaque dolore, quam fuga iusto voluptatem odio tenetur dignissimos reiciendis molestias maiores? Esse nesciunt cum sint ipsam autem molestiae porro quod tenetur veritatis ratione quasi libero vitae, voluptatibus, ad quibusdam. Quos dolorum labore fugiat deserunt totam dolor voluptatibus quidem veritatis distinctio quia accusamus maxime nesciunt unde officiis similique iusto earum ipsa, eveniet eligendi dolores. Sunt quibusdam adipisci, suscipit nisi repellendus consequatur odio totam ullam explicabo voluptates voluptatem doloremque, aperiam modi saepe nam!",
+				},
+				{
+					title: "Dicuss with clients",
+					person: "The Net Ninja",
+					due: "March 2022",
+					status: "overdue",
+					content:
+						"Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi assumenda cupiditate, reiciendis suscipit quod laudantium, architecto itaque, odio ab beatae sapiente fugit. Sed id deleniti autem? Facere, doloribus eaque dolore, quam fuga iusto voluptatem odio tenetur dignissimos reiciendis molestias maiores? Esse nesciunt cum sint ipsam autem molestiae porro quod tenetur veritatis ratione quasi libero vitae, voluptatibus, ad quibusdam. Quos dolorum labore fugiat deserunt totam dolor voluptatibus quidem veritatis distinctio quia accusamus maxime nesciunt unde officiis similique iusto earum ipsa, eveniet eligendi dolores. Sunt quibusdam adipisci, suscipit nisi repellendus consequatur odio totam ullam explicabo voluptates voluptatem doloremque, aperiam modi saepe nam!",
+				},
+			],
+		};
+	},
+	computed: {
+		myProjects() {
+			return this.projects.filter((p) => {
+				return p.person === "The Net Ninja";
+			});
+		},
+	},
 };
 </script>
 <style lang=""></style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,11 +1,83 @@
-<template lang="">
-  <div>
-    <h1>Team</h1>
+<template>
+  <div class="mt-4 ml-4">
+    <h1 class="subheader grey--text">
+      Team
+    </h1>
+
+    <v-container class="my-5">
+      <v-row>
+        <v-col 
+            v-for="person in team"
+            :key="person.name"
+            cols="12"
+            xs="12"
+            sm="6"
+            md="4"
+            lg="3"
+        >
+          <v-card
+              
+              class="text-center ma-3"
+          >
+            <v-responsive class="pt-4">
+              <v-avatar
+                  size="100"
+                  class="grey lighten-2"
+              >
+                <img :src="person.avatar">
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="text-subtitle-1">
+                {{ person.name }}
+              </div>
+              <div class="subheading grey--text">
+                {{ person.role }}
+              </div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                  text
+                  color="grey"
+              >
+                <v-icon
+                    small
+                    left
+                >
+                  mdi-message
+                </v-icon>
+                <span>message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script>
 export default {
-  name: "TeamView",
+	name: "TeamView",
+	data() {
+		return {
+			team: [
+				{
+					name: "The Net Ninja",
+					role: "Web developer",
+					avatar: "/avatar-1.png",
+				},
+				{ name: "Ryu", role: "Graphic designer", avatar: "/avatar-2.png" },
+				{ name: "Chun Li", role: "Web developer", avatar: "/avatar-3.png" },
+				{
+					name: "Gouken",
+					role: "Social media maverick",
+					avatar: "/avatar-4.png",
+				},
+				{ name: "Yoshi", role: "Sales guru", avatar: "/avatar-5.png" },
+			],
+		};
+	},
 };
 </script>
 <style lang=""></style>
